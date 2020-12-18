35a50f5d34b7786cf426f22792574e4d50951274
root <goknar@gmail.com>
2020-12-18 14:32:43 +0000

    movie_script persona maker

--- train.py
+++ train.py
@@ -75,6 +75,7 @@ def get_data_loaders(args, tokenizer):
     logger.info("Build inputs and labels")
     datasets = {"train": defaultdict(list), "valid": defaultdict(list)}
     for dataset_name, dataset in personachat.items():
+        print(dataset[0]["utterances"][0])
         num_candidates = len(dataset[0]["utterances"][0]["candidates"])
         if args.num_candidates > 0 and dataset_name == 'train':
             num_candidates = min(args.num_candidates, num_candidates)
